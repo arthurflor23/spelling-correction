0cdde6e31a11fa69836578dfd1ca91de1f1d2f3b
Arthur Flôr <arthurflor23@gmail.com>
2020-05-02 11:22:54 -0300

    Add kaldi function

--- src/tool/statistical.py
+++ src/tool/statistical.py
@@ -19,10 +19,9 @@ from symspellpy.symspellpy import SymSpell
 
 class LanguageModel():
 
-    def __init__(self, mode, source, output, N=2):
+    def __init__(self, mode, output, N=2):
         self.autocorrect = getattr(self, f"_{mode}")
-        self.source = source
-        self.output = output
+        self.output_path = output
         self.N = N
 
     def create_corpus(self, sentences):
@@ -30,9 +29,9 @@ class LanguageModel():
 
         matches = " ¶ ".join(sentences).translate(str.maketrans("", "", string.punctuation))
         matches = re.compile(r'[^\S\n]+', re.UNICODE).sub(" ", matches.strip())
-        matches = "\n".join(matches.strip().split(" ¶ "))
+        matches = "\n".join(matches.strip().split(" ¶ ")).lower()
 
-        return matches.lower()
+        return matches
 
     def read_corpus(self, corpus_path):
         """Read corpus file to the autocorrect tool"""
@@ -40,6 +39,51 @@ class LanguageModel():
         self.corpus_path = corpus_path
         self.dictionary_path = os.path.join(os.path.dirname(corpus_path), "dictionary.txt")
         self.corpus = " ".join(open(corpus_path).read().splitlines()).lower()
+
+    def _kaldi(self, sentences, predict=True):	
+        """	
+        Kaldi Speech Recognition Toolkit with SRI Language Modeling Toolkit.	
+        ** Important Note **	
+        You'll need to do all by yourself:	
+        1. Compile Kaldi with SRILM and OpenBLAS.	
+        2. Create and add kaldi folder in the project `lib` folder (``src/lib/kaldi/``)	
+        3. Generate files (search `--kaldi_assets` in https://github.com/arthurflor23/handwritten-text-recognition):	
+            a. `chars.lst`	
+            b. `conf_mats.ark`	
+            c. `ground_truth.lst`	
+            d. `ID_test.lst`	
+            e. `ID_train.lst`	
+        4. Add files (item 3) in the project `output` folder: ``output/<DATASET>/kaldi/``	
+        More information (maybe help) in ``src/lib/kaldi-decode-script.sh`` comments.	
+        References:	
+            D. Povey, A. Ghoshal, G. Boulianne, L. Burget, O. Glembek, N. Goel, M. Hannemann,	
+            P. Motlicek, Y. Qian, P. Schwarz, J. Silovsky, G. Stem- mer and K. Vesely.	
+            The Kaldi speech recognition toolkit, 2011.	
+            Workshop on Automatic Speech Recognition and Understanding.	
+            URL: http://github.com/kaldi-asr/kaldi	
+            Andreas Stolcke.	
+            SRILM - An Extensible Language Modeling Toolkit, 2002.	
+            Proceedings of the 7th International Conference on Spoken Language Processing (ICSLP).	
+            URL: http://www.speech.sri.com/projects/srilm/	
+        """	
+
+        option = "TEST" if predict else "TRAIN"	
+
+        if os.system(f"./lib/kaldi-decode-script.sh {self.output_path} {option} {self.N}") != 0:	
+            print("\n##########################################\n")	
+            print("You'll have to work hard for this option.\n")	
+            print("See some instructions in the ``src/tool/statistical.py`` file (kaldi function section)")	
+            print("and also in the ``src/lib/kaldi-decode-script.sh`` file. \n☘️ ☘️ ☘️")	
+            print("\n##########################################\n")	
+
+        if predict:	
+            predicts = open(os.path.join(self.output_path, "data", "predicts_t")).read().splitlines()	
+
+            for i, line in enumerate(predicts):	
+                tokens = line.split()	
+                predicts[i] = "".join(tokens[1:]).replace("<space>", " ").strip()	
+
+            return predicts
 
     def _similarity(self, sentences):
         """
